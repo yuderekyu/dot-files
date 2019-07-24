3e2a12287613d7cb7019305da00ffb425b67f71e
Derek Yu <yuderekyu@gmail.com>
2019-07-24 15:21:21 -0500

    remove random code i found from the interweb.

--- init.el
+++ init.el
@@ -31,6 +31,7 @@ values."
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
    '(
+     javascript
      ;; ----------------------------------------------------------------
      ;; Example of useful layers you may want to use right away.
      ;; Uncomment some layer names and press <SPC f e R> (Vim style) or
@@ -319,70 +320,6 @@ This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
   (add-to-list 'spacemacs-indent-sensitive-modes 'idris-mode)
-  (defun with-face (str &rest face-plist)
-    (propertize str 'face face-plist))
-  esetq esh(setq eshell-history-size 1024)
-
-(load "em-hist")           ; So the history vars are defined
-(if (boundp 'eshell-save-history-on-exit)
-    (setq eshell-save-history-on-exit t)) ; Don't ask, just save
-;(message "eshell-ask-to-save-history is %s" eshell-ask-to-save-history)
-(if (boundp 'eshell-ask-to-save-history)
-    (setq eshell-ask-to-save-history 'always)) ; For older(?) version
-;(message "eshell-ask-to-save-history is %s" eshell-ask-to-save-history)
-
-(defun eshell/ef (fname-regexp &rest dir) (ef fname-regexp default-directory))
-
-
-;;; ---- path manipulation
-
-(defun pwd-repl-home (pwd)
-  (interactive)
-  (let* ((home (expand-file-name (getenv "HOME")))
-   (home-len (length home)))
-    (if (and
-   (>= (length pwd) home-len)
-   (equal home (substring pwd 0 home-len)))
-  (concat "~" (substring pwd home-len))
-      pwd)))
-
-(defun curr-dir-git-branch-string (pwd)
-  "Returns current git branch as a string, or the empty string if
-PWD is not in a git repo (or the git command is not found)."
-  (interactive)
-  (when (and (eshell-search-path "git")
-             (locate-dominating-file pwd ".git"))
-    (let ((git-output (shell-command-to-string (concat "cd " pwd " && git branch | grep '\\*' | sed -e 's/^\\* //'"))))
-      (propertize (concat "["
-              (if (> (length git-output) 0)
-                  (substring git-output 0 -1)
-                "(no branch)")
-              "]") 'face `(:foreground "green"))
-      )))
-
-(setq eshell-prompt-function
-      (lambda ()
-        (concat
-         (propertize ((lambda (p-lst)
-            (if (> (length p-lst) 3)
-                (concat
-                 (mapconcat (lambda (elm) (if (zerop (length elm)) ""
-                                            (substring elm 0 1)))
-                            (butlast p-lst 3)
-                            "/")
-                 "/"
-                 (mapconcat (lambda (elm) elm)
-                            (last p-lst 3)
-                            "/"))
-              (mapconcat (lambda (elm) elm)
-                         p-lst
-                         "/")))
-          (split-string (pwd-repl-home (eshell/pwd)) "/")) 'face `(:foreground "yellow"))
-         (or (curr-dir-git-branch-string (eshell/pwd)))
-         (propertize "# " 'face 'default))))ell-prompt-function 'curr-dir-git-branch-string)
-  (setq eshell-highlight-prompt nil)
-  (global-git-commit-mode t)
-  )
 
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
