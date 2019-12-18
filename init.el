5a8a4095325b7f6c46d8215f54962e8a764ce0b1
Derek Yu <yuderekyu@gmail.com>
2019-12-18 14:02:22 -0600

    set x-term-mouse-mode to -1. allows seamless cursor highlight for copy and paste.

--- init.el
+++ init.el
@@ -324,6 +324,9 @@ This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
 
+  ;; copy highlighted text
+  (xterm-mouse-mode -1)
+
   ;; globally turn on word wrap.
   (global-visual-line-mode t)
 
